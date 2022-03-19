539a6c63081542c3fa7a050dcd3345e243c7eca5
Carlos Barrera <carlos.barrera@ceiba.com.co>
2022-03-19 18:42:06 -0500

    Ajuste de deuda técnica

---
+++ presupuesto/dominio/src/test/java/com/ceiba/presupuesto/servicio/ServicioActualizarUsuarioTest.java
@@ -0,0 +1,49 @@
+package com.ceiba.presupuesto.servicio;
+
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
+import com.ceiba.presupuesto.modelo.entidad.Presupuesto;
+import com.ceiba.presupuesto.puerto.repositorio.RepositorioPresupuesto;
+import com.ceiba.presupuesto.servicio.testdatabuilder.PresupuestoTestDataBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ServicioActualizarPresupusetoTest {
+
+    @Test
+    @DisplayName("Deberia actualizar actualizar el presupuesto")
+    void deberiaActualizarCorrectamenteEnElRepositorio() {
+        Presupuesto presupuesto = new PresupuestoTestDataBuilder()
+                .conId(1L)
+                .conValorPresupuesto(8000L)
+                .build();
+        RepositorioPresupuesto repositorioPresupuesto = Mockito.mock(RepositorioPresupuesto.class);
+        Mockito.when(repositorioPresupuesto.existeUsuario(Mockito.anyString())).thenReturn(true);
+        Mockito.when(repositorioPresupuesto.existe(Mockito.anyLong())).thenReturn(true);
+        ServicioActualizarPresupuesto servicioActualizarPresupuesto = new ServicioActualizarPresupuesto(repositorioPresupuesto);
+        // act
+        servicioActualizarPresupuesto.ejecutar(presupuesto);
+        //- assert
+        Mockito.verify(repositorioPresupuesto, Mockito.times(1)).actualizar(presupuesto);
+    }
+
+    @Test
+    @DisplayName("Deberia validar la existencia previa del usuario")
+    void deberiaValidarLaExistenciaPreviaDelUsuario() {
+        // arrange
+        Presupuesto presupuesto = new PresupuestoTestDataBuilder().conIdentificacionUsuario("94").build();
+        RepositorioPresupuesto repositorioPresupuesto = Mockito.mock(RepositorioPresupuesto.class);
+        Mockito.when(repositorioPresupuesto.existeUsuario(Mockito.anyString())).thenReturn(true);
+        Mockito.when(repositorioPresupuesto.existePresupuesto(Mockito.anyString(), Mockito.any(LocalDateTime.class))).thenReturn(false);
+        ServicioActualizarPresupuesto servicioActualizarPresupuesto = new ServicioActualizarPresupuesto(repositorioPresupuesto);
+        // act - assert
+        BasePrueba.assertThrows(() ->
+                servicioActualizarPresupuesto.ejecutar(presupuesto)
+                , ExcepcionSinDatos.class,"El presupuesto no existe en el sistema");
+    }
+}
--- presupuesto/dominio/src/test/java/com/ceiba/presupuesto/servicio/ServicioCrearPresupuestoTest.java
+++ presupuesto/dominio/src/test/java/com/ceiba/presupuesto/servicio/ServicioCrearPresupuestoTest.java
@@ -3,10 +3,13 @@ package com.ceiba.presupuesto.servicio;
 import com.ceiba.BasePrueba;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
 import com.ceiba.dominio.excepcion.ExcepcionLongitudValor;
+import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
 import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.presupuesto.modelo.entidad.Presupuesto;
 import com.ceiba.presupuesto.puerto.repositorio.RepositorioPresupuesto;
 import com.ceiba.presupuesto.servicio.testdatabuilder.PresupuestoTestDataBuilder;
+import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
+import com.ceiba.usuario.servicio.ServicioCrearUsuario;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,51 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ServicioCrearPresupuestoTest {
+
+    @Test
+    @DisplayName("Deberia Crear el presupuseto de manera correcta")
+    void deberiaCrearPresupuesto() {
+        // arrange
+        Presupuesto presupuesto = new PresupuestoTestDataBuilder().build();
+        RepositorioPresupuesto repositorioPresupuesto = Mockito.mock(RepositorioPresupuesto.class);
+        Mockito.when(repositorioPresupuesto.existeUsuario(Mockito.anyString())).thenReturn(true);
+        Mockito.when(repositorioPresupuesto.existePresupuesto(Mockito.anyString(), Mockito.any(LocalDateTime.class))).thenReturn(false);
+        ServicioCrearPresupuesto servicioCrearPresupuesto = new ServicioCrearPresupuesto(repositorioPresupuesto);
+        // act
+        Long idPresupuesto = servicioCrearPresupuesto.ejecutar(presupuesto);
+        //- assert
+        assertEquals(0L,idPresupuesto);
+        Mockito.verify(repositorioPresupuesto, Mockito.times(1)).crear(presupuesto);
+    }
+
+    @Test
+    @DisplayName("No deberia Crear el presupuseto de manera correcta dado que no existe usuario")
+    void NodeberiaCrearPresupuestoDadoQueNoExisteUsuario() {
+        // arrange
+        Presupuesto presupuesto = new PresupuestoTestDataBuilder().build();
+        RepositorioPresupuesto repositorioPresupuesto = Mockito.mock(RepositorioPresupuesto.class);
+        Mockito.when(repositorioPresupuesto.existeUsuario(Mockito.anyString())).thenReturn(false);
+        ServicioCrearPresupuesto servicioCrearPresupuesto = new ServicioCrearPresupuesto(repositorioPresupuesto);
+        // act - assert
+        BasePrueba.assertThrows(() ->
+                servicioCrearPresupuesto.ejecutar(presupuesto)
+                , ExcepcionSinDatos.class,"El usuario no existe");;
+    }
+
+    @Test
+    @DisplayName("No deberia Crear el presupuseto dado que ya existe")
+    void NodeberiaCrearPresupuestoDadoQueYaExiste() {
+        // arrange
+        Presupuesto presupuesto = new PresupuestoTestDataBuilder().build();
+        RepositorioPresupuesto repositorioPresupuesto = Mockito.mock(RepositorioPresupuesto.class);
+        Mockito.when(repositorioPresupuesto.existeUsuario(Mockito.anyString())).thenReturn(true);
+        Mockito.when(repositorioPresupuesto.existePresupuesto(Mockito.anyString(), Mockito.any(LocalDateTime.class))).thenReturn(true);
+        ServicioCrearPresupuesto servicioCrearPresupuesto = new ServicioCrearPresupuesto(repositorioPresupuesto);
+        // act - assert
+        BasePrueba.assertThrows(() ->
+                        servicioCrearPresupuesto.ejecutar(presupuesto)
+                , ExcepcionDuplicidad.class,"El presupuesto ya existe en el sistema");;
+    }
 
     @Test
     @DisplayName("Deberia lanzar una exepecion cuando indentificacionUsuario mayor a 15")
